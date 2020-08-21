32898cb883554871a36160311f19960be291cb96
Tayman Ott <taymanjott@gmail.com>
2020-08-21 13:53:55 -0500

    added spots

--- src/map.js
+++ src/map.js
@@ -16,8 +16,8 @@ import * as mapboxgl from 'mapbox-gl';
         const map = new mapboxgl.Map({
             container: this.mapRef.current,
             style:'mapbox://styles/taymanott/cke3jbav2092j19rxk8oq64zb',
-            center:[-89.376,43.082],
-            zoom:14.36
+            center:[-89.383,43.074],
+            zoom:15.36
         });
 
         var geojson = {
@@ -50,13 +50,73 @@ import * as mapboxgl from 'mapbox-gl';
             type: 'Feature',
             geometry: {
                 type:'point',
-                coordinates: [-89.404081, 43.5650409]
+                coordinates: [-89.4003387, 43.0745857]
             },
         
             properties: {
-                title: 'Memorial High',
-                description: 'Ledges and some bleachers'
-        }}] 
+                title: 'Legendary Spot',
+                description: 'Ledges'
+        }},
+        {
+            type:'Feature',
+            geometry:{
+                type:'point',
+                coordinates: [-89.3880214,43.070965]
+            },
+            properties:{
+                title:'Lunar Court',
+                description: 'Aluminum ledges'
+            }
+        },
+        {
+            type:'Feature',
+            geometry:{
+                type:'point',
+                coordinates:[-89.398481,43.0689118]
+            },
+            properties:{
+                title:'Big C',
+                description:'Large C ledge'
+            }
+            
+        },
+        {
+            type: 'Feature',
+            geometry:{
+                type: 'point',
+                coordinates:[-89.3853247,43.0756964]
+            },
+            properties:{
+                title: 'Bulletproof Glass',
+                description: 'Manual Pad/Walride'
+            }
+            
+        },
+        {
+            type: 'Feature',
+            geometry:{
+                type: 'point',
+                coordinates:[-89.3804286,43.0762359]
+            },
+            properties:{
+                title: 'The Best Slappy in town',
+                description: 'curb'
+            }
+            
+        },
+        {
+            type: 'Feature',
+            geometry:{
+                type: 'point',
+                coordinates:[-89.3804286,43.0762359]
+            },
+            properties:{
+                title: 'The Garden',
+                description: 'climb the hill and find out'
+            }
+            
+        },
+    ] 
         };
         
         var shops = {
