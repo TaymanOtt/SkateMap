5eae087948f697c7da36af783572a79c9eb46bd6
Tayman Ott <taymanjott@gmail.com>
2020-08-24 21:13:34 -0500

    added paypal

--- src/Merch.js
+++ src/Merch.js
@@ -11,7 +11,13 @@ function Card(){
     return(<div className = "merch">
            <h5>Merch</h5>
            <div>
-              
+           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="RFBKWA8KY9B4A" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form>
+ 
            </div>
            <p>donate</p>
            <button onClick = {close}>X</button>
