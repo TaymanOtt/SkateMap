024ac54839b8f9711a4cd9a97840a4e4ba9c658b
TaymanOtt <taymantott@gmail.com>
2020-12-02 19:54:15 -0600

    styled buttons

--- src/key.js
+++ src/key.js
@@ -3,21 +3,7 @@ import {close} from './close.js'
 function Key(){
     return(
         <div className = "key">
-            <h2>Key</h2>
-            <div className='keyList'>
-                <div className ='keyListItem'>
-                    <p>Skatepark</p>
-                    <img src='https://i.ibb.co/FBPWF2d/park.jpg' alt='park logo'/>
-                 </div>
-                <div className='keyListItem'>
-                    <p>SkateSpot</p>
-                    <img src ='https://i.ibb.co/x1dR6Fw/helmet.jpg' alt='skatespot'/>
-                </div>
-                
-            </div>
-            <div className='closeBtn'>
-            <button onClick={close}>X</button>
-            </div>
+           <h1>Add Spots</h1>
         </div>
     )
     
